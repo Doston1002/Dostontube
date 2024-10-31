cee9995bf8a86442cd28f6f1f3b1efdd57f3af33
Doston1002 <polatovdoston1002>
2024-10-31 17:16:40 +0500

    full project

--- dostontubu/src/service/api.service.js
+++ dostontubu/src/service/api.service.js
@@ -1,21 +1,21 @@
 import axios from 'axios'
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+
+const BASE_URI = 'https://youtube-v31.p.rapidapi.com'
+const RAPID_API_KEY = process.env.REACT_APP_PUBLIC_KEY
 
 const options = {
-    params: {
-      maxResults: '50'
-    },
-    headers: {
-        'x-rapidapi-key': 'ab12fa5a0dmsh78f42e2188a3f52p139cd4jsne9777998dcaa',
-        'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
-      }
-  };
+	params: {
+		maxResults: '50',
+	},
+	headers: {
+		'X-RapidAPI-Key': RAPID_API_KEY.toString(),
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+	},
+}
 
-
-
-  export const ApiService = {
-    async fetching(url){
-const response = await axios.get(`${BASE_URL}/${url}`, options)
-return response.data
-    }
-  }
+export const ApiService = {
+	async fetching(url) {
+		const response = await axios.get(`${BASE_URI}/${url}`, options)
+		return response.data
+	},
+}
